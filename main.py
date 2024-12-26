d217c57b04d5ea74cfb3a2dd2a450e2201c464ac
AbuHamse <dirie.a99@gmail.com>
2024-12-26 18:19:16 -0500

    Exercise 1

---
+++ main.py
@@ -0,0 +1,11 @@
+operation = input('Please eneter in an operator(+, -, /, *): ')
+num1 = int(input('Please enter a number: '))
+num2 = int(input('Please enter a number: '))
+
+
+if operation == '+':
+    results = num1 + num2 
+    print(results);
+    elss operation == '/':
+        results = num1 / num2
+        print(results);
