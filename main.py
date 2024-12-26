aa3419a9ea867b7927e5eb4377f1e23399b9e47e
AbuHamse <dirie.a99@gmail.com>
2024-12-26 18:23:34 -0500

    Fixed Exercise One

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-operation = input('Please eneter in an operator(+, -, /, *): ')
+operation = input('Please enter an operator (+, -, /, *): ')
 num1 = int(input('Please enter a number: '))
-num2 = int(input('Please enter a number: '))
-
+num2 = int(input('Please enter another number: '))
 
 if operation == '+':
-    results = num1 + num2 
-    print(results);
-    elss operation == '/':
+    results = num1 + num2
+    print(f"The result is: {results}")
+elif operation == '-':
+    results = num1 - num2
+    print(f"The result is: {results}")
+elif operation == '/':
+    if num2 != 0:
         results = num1 / num2
-        print(results);
+        print(f"The result is: {results}")
+    else:
+        print("Error: Division by zero is not allowed.")
+elif operation == '*':
+    results = num1 * num2
+    print(f"The result is: {results}")
+else:
+    print(f"Please enter a vaild operator {operation}")
